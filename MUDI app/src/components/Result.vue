ec0de849af56f7967e30bbf26b1101a3dd5b422f
Mudi Ogra <ograabiodun@gmail.com>
2019-11-22 16:46:31 +0100

    made changes

--- MUDI app/src/components/Result.vue
+++ MUDI app/src/components/Result.vue
@@ -1,24 +1,4 @@
 <template>
-  <!-- <div class="col-md-4 mb-3">
-    <div class="card" style="width: 18rem;">
-      <img src="@/assets/placeholder.png" class="card-img-top" alt="@/assets/placeholder.png">
-      <div class="card-body">
-        <h5 class="card-title">{{result ? result.title : null}} <span
-            class="badge badge-warning">{{result.type}}</span>
-        </h5>
-        <p class="card-text">{{ result.description }} .. {{result.area}}</p>
-        <div class="bottom">
-          <a @click.prevent="reviews" class="btn btn-primary text-white">See reviews</a>
-          <span class="">{{ '$' + result.price }}</span>
-          <span
-              style="background: url('/star.svg') center center/cover; width: 30px; height: 30px; display:flex; justify-content: center; align-content: center"> {{result.rating}} </span>
-        </div>
-        <ul class="list-group list-group-flush mt-2">
-          <li class="list-group-item">Created by: {{ result.author.firstname + " "+ result.author.lastname }}</li>
-        </ul>
-      </div>
-    </div>
-  </div> -->
 
   <div class="col-md-4 mb-9" >
   <section class="hoc container clear"> 
